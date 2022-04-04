2065fde240a9fc7b0fe5a45d5d7d87457cdcd793
Crucial Jun <nich.otieno@gmail.com>
2022-04-04 20:03:30 +0300

    remove unused textfields

--- lib/screens/appointment_booking_screen.dart
+++ lib/screens/appointment_booking_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:dentmind_dental_centre/app_colors.dart';
-import 'package:dentmind_dental_centre/utils/text_field_validators.dart';
 import 'package:dentmind_dental_centre/utils/text_form_decoration.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
@@ -19,9 +18,6 @@ class AppoitnmentBooking extends StatefulWidget {
 }
 
 class _AppoitnmentBookingState extends State<AppoitnmentBooking> {
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
   final List<String> branches = [
     'Kitengela',
     'Mombasa Road',
@@ -43,9 +39,6 @@ class _AppoitnmentBookingState extends State<AppoitnmentBooking> {
 
   @override
   void dispose() {
-    _firstNameController.dispose();
-    _lastNameController.dispose();
-    _emailController.dispose();
     super.dispose();
   }
 
@@ -58,234 +51,203 @@ class _AppoitnmentBookingState extends State<AppoitnmentBooking> {
         foregroundColor: primaryAppColor,
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(bottom: 16, left: 16, right: 16),
-        child: Column(
-          children: [
-            Expanded(child: SvgPicture.asset("assets/appointment.svg")),
-            const Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Book an Appoitment",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: primaryAppColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32),
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 16, left: 16, right: 16),
+          child: Column(
+            children: [
+              SizedBox(
+                  height: 240,
+                  child: SvgPicture.asset("assets/appointment.svg")),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Book an Appoitment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: primaryAppColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32),
+                ),
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    validator: (value) => firstNameValidator(value),
-                    keyboardType: TextInputType.name,
-                    controller: _firstNameController,
-                    decoration:
-                        const TextFormDecoration(labelString: "First name"),
-                  ),
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    validator: (value) => lastNameValidator(value),
-                    keyboardType: TextInputType.name,
-                    controller: _lastNameController,
-                    decoration:
-                        const TextFormDecoration(labelString: "Last name"),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              validator: (value) => emailValidator(value),
-              keyboardType: TextInputType.name,
-              controller: _lastNameController,
-              decoration: const TextFormDecoration(labelString: "Email"),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            IntlPhoneField(
-              initialCountryCode:
-                  WidgetsBinding.instance!.window.locale.countryCode,
-              decoration: const TextFormDecoration(labelString: "Phone Number"),
-            ),
-            DropdownButtonFormField2(
-              focusColor: Colors.white,
-              value: selectedBranch,
-              items: branches
-                  .map((e) => DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
-                      ))
-                  .toList(),
-              decoration:
-                  const TextFormDecoration(labelString: "Choose Branch"),
-              validator: (value) {
-                if (value == null) {
-                  return 'Please Select Branch';
-                }
-                return null;
-              },
-              onChanged: (value) {
-                //Do something when changing the item if you want.
-              },
-              onSaved: (value) {
-                selectedBranch = value.toString();
-              },
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            DropdownButtonFormField2(
-              focusColor: Colors.white,
-              value: selectedService,
-              items: listOfServices
-                  .map((e) => DropdownMenuItem(
-                        value: e,
-                        child: Text(e),
-                      ))
-                  .toList(),
-              decoration:
-                  const TextFormDecoration(labelString: "Choose Service"),
-              validator: (value) {
-                if (value == null) {
-                  return 'Please Select Service';
-                }
-                return null;
-              },
-              onChanged: (value) {
-                //Do something when changing the item if you want.
-              },
-              onSaved: (value) {
-                selectedBranch = value.toString();
-              },
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Pick a date for the appointment',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const SizedBox(
-                  width: 24,
-                ),
-                TextButton(
-                    onPressed: () async {
-                      var dateSelected = await showDatePicker(
-                          context: context,
-                          initialDate: _selectedDate,
-                          firstDate: DateTime(2022),
-                          lastDate: DateTime(2030));
-
-                      if (dateSelected != null &&
-                          dateSelected != _selectedDate) {
-                        setState(() {
-                          _selectedDate = dateSelected;
-                        });
-                      }
-                    },
-                    child: Text(
-                        "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}",
-                        style: const TextStyle(
-                          color: primaryAppColor,
-                          fontWeight: FontWeight.w600,
-                        ))),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Pick a date for the appointment',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const SizedBox(
-                  width: 24,
-                ),
-                TextButton(
-                    onPressed: () async {
-                      var timeSelected = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.fromDateTime(_selectedDate));
-
-                      if (timeSelected != null &&
-                          timeSelected != _selectedTime) {
-                        setState(() {
-                          _selectedTime = timeSelected;
-                        });
-                      }
-                    },
-                    child: Text("${_selectedTime.hour}:${_selectedTime.minute}",
-                        style: const TextStyle(
-                          color: primaryAppColor,
-                          fontWeight: FontWeight.w600,
-                        ))),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Text("Enter additional appointment details",
-                style: TextStyle(
-                  color: primaryAppColor,
-                  fontWeight: FontWeight.w600,
-                )),
-            const TextField(
-              keyboardType: TextInputType.multiline,
-              maxLength: null,
-              maxLines: null,
-              decoration: TextFormDecoration(labelString: ""),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: accentAppColor,
+              IntlPhoneField(
+                initialCountryCode:
+                    WidgetsBinding.instance!.window.locale.countryCode,
+                decoration:
+                    const TextFormDecoration(labelString: "Phone Number"),
+              ),
+              DropdownButtonFormField2(
+                focusColor: Colors.white,
+                value: selectedBranch,
+                items: branches
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        ))
+                    .toList(),
+                decoration:
+                    const TextFormDecoration(labelString: "Choose Branch"),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Select Branch';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  //Do something when changing the item if you want.
+                },
+                onSaved: (value) {
+                  selectedBranch = value.toString();
+                },
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              DropdownButtonFormField2(
+                focusColor: Colors.white,
+                value: selectedService,
+                items: listOfServices
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        ))
+                    .toList(),
+                decoration:
+                    const TextFormDecoration(labelString: "Choose Service"),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please Select Service';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  //Do something when changing the item if you want.
+                },
+                onSaved: (value) {
+                  selectedBranch = value.toString();
+                },
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'Pick a date for the appointment',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Cancel",
-                          style: TextStyle(
-                            color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        var dateSelected = await showDatePicker(
+                            context: context,
+                            initialDate: _selectedDate,
+                            firstDate: DateTime(2022),
+                            lastDate: DateTime(2030));
+
+                        if (dateSelected != null &&
+                            dateSelected != _selectedDate) {
+                          setState(() {
+                            _selectedDate = dateSelected;
+                          });
+                        }
+                      },
+                      child: Text(
+                          "${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}",
+                          style: const TextStyle(
+                            color: primaryAppColor,
                             fontWeight: FontWeight.w600,
-                          )),
-                    )),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: primaryAppColor,
+                          ))),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'Pick a date for the appointment',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Book Now",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                          )),
-                    )),
-              ],
-            ),
-          ],
+                  ),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        var timeSelected = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.fromDateTime(_selectedDate));
+
+                        if (timeSelected != null &&
+                            timeSelected != _selectedTime) {
+                          setState(() {
+                            _selectedTime = timeSelected;
+                          });
+                        }
+                      },
+                      child:
+                          Text("${_selectedTime.hour}:${_selectedTime.minute}",
+                              style: const TextStyle(
+                                color: primaryAppColor,
+                                fontWeight: FontWeight.w600,
+                              ))),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Text("Enter additional appointment details",
+                  style: TextStyle(
+                    color: primaryAppColor,
+                    fontWeight: FontWeight.w600,
+                  )),
+              const TextField(
+                keyboardType: TextInputType.multiline,
+                maxLength: null,
+                maxLines: null,
+                decoration: TextFormDecoration(labelString: ""),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: accentAppColor,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Cancel",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            )),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: primaryAppColor,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Book Now",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            )),
+                      )),
+                ],
+              ),
+            ],
+          ),
         ),
       )),
     );
